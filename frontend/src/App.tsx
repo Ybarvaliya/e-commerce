cf635abca1bf84289e4f06c44f45e205431eb2bb
Ybarvaliya <uvlbarvaliya@gmail.com>
2024-01-12 20:28:36 +0530

    folder strcture

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { lazy, Suspense } from "react";
+import Loader from "./components/Loader";
 
+const Home = lazy(() => import("./pages/Home"));
+const Cart = lazy(() => import("./pages/Cart"));
+const Search = lazy(() => import("./components/Search"));
 
 function App() {
-  
-
   return (
     <>
-      <div>
-        HELLO
-      </div>
+      <Router>
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route path="/" element={<Home></Home>}></Route>
+            <Route path="/cart" element={<Cart></Cart>}></Route>
+            <Route path="/search" element={<Search></Search>}></Route>
+          </Routes>
+        </Suspense>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
