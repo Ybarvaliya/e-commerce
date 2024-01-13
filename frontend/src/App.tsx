09089bf44e0f0dac581df1a8dac4bd1200fbd2d5
Ybarvaliya <uvlbarvaliya@gmail.com>
2024-01-13 12:27:03 +0530

    admin routes created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,13 @@ const Home = lazy(() => import("./pages/Home"));
 const Cart = lazy(() => import("./pages/Cart"));
 const Search = lazy(() => import("./components/Search"));
 
+// Admin Panel
+
+const Dashboard = lazy(() => import("./pages/admin/Dashboard"));
+const Products = lazy(() => import("./pages/admin/Products"));
+const Transactions = lazy(() => import("./pages/admin/Transactions"));
+const Customers = lazy(() => import("./pages/admin/Customers"));
+
 function App() {
   return (
     <>
@@ -15,6 +22,13 @@ function App() {
             <Route path="/" element={<Home></Home>}></Route>
             <Route path="/cart" element={<Cart></Cart>}></Route>
             <Route path="/search" element={<Search></Search>}></Route>
+
+            {/* Admin Panel */}
+
+            <Route path="/admin/dashboard" element={<Dashboard></Dashboard>}></Route>
+            <Route path="/admin/product" element={<Products></Products>}></Route>
+            <Route path="/admin/customer" element={<Customers></Customers>}></Route>
+            <Route path="/admin/transaction" element={<Transactions></Transactions>}></Route>
           </Routes>
         </Suspense>
       </Router>
