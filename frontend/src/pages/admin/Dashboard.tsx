0d67879659b986629897392d9b5a2089e4a7d82d
Ybarvaliya <uvlbarvaliya@gmail.com>
2024-01-17 10:05:42 +0530

    admin - dashboard - Inventory created

--- frontend/src/pages/admin/Dashboard.tsx
+++ frontend/src/pages/admin/Dashboard.tsx
@@ -9,7 +9,6 @@ const Dashboard = () => {
     <div className="flex">
       <AdminSidebar></AdminSidebar>
       <main className="mt-5 mx-2 rounded-lg bg-slate-300 w-full">
-
         {/* searchBar */}
 
         <div className="flex">
@@ -36,6 +35,13 @@ const Dashboard = () => {
           ></SmallCard>
           <SmallCard title="Products" num="1000" percentage={30}></SmallCard>
         </div>
+
+        {/* R&T Chart & Inventory */}
+
+        <div className="flex">
+          <RTChart></RTChart>
+          <Inventory></Inventory>
+        </div>
       </main>
     </div>
   );
@@ -56,7 +62,7 @@ const SmallCard = ({ title, num, percentage }: smallCardProps) => {
     <div className="bg-white p-2 m-5 rounded-md flex flex-shrink-0">
       <div className="ml-3 mt-5 mr-2">
         <div className="m-1 text-slate-700">{title}</div>
-        <div className="text-xl m-1">{num}</div>
+        <div className="text-xl m-1 font-semibold">{num}</div>
       </div>
 
       {percentage > 0 ? (
@@ -92,6 +98,72 @@ const Disk = ({ percentage, color }: diskProp) => {
         )}
         {percentage}%
       </div>
+    </div>
+  );
+};
+
+const RTChart = () => {
+  return (
+    <div className="bg-white h-60 w-4/6 rounded-lg m-2 ml-5">
+      <h1 className="text-center text-xl m-2 font-semibold">
+        Revenue & Transactions
+      </h1>
+    </div>
+  );
+};
+
+const Inventory = () => {
+  return (
+    <div className="bg-white w-1/4 rounded-lg m-2 ml-5">
+      <h1 className="text-center text-xl m-2 font-semibold">Inventory</h1>
+      <div className="m-4">
+        <InventoryItem
+          heading="Laptops"
+          color="rgb(137, 196, 244)"
+          value={12}
+        ></InventoryItem>
+        <InventoryItem
+          heading="Shoes"
+          color="rgb(137, 196, 244)"
+          value={40}
+        ></InventoryItem>
+        <InventoryItem
+          heading="Camera"
+          color="rgb(137, 196, 244)"
+          value={57}
+        ></InventoryItem>
+        <InventoryItem
+          heading="Geans"
+          color="rgb(137, 196, 244)"
+          value={80}
+        ></InventoryItem>
+        <InventoryItem
+          heading="Shirts"
+          color="rgb(137, 196, 244)"
+          value={100}
+        ></InventoryItem>
+      </div>
+    </div>
+  );
+};
+
+interface InventoryProps {
+  heading: string;
+  color: string;
+  value: number;
+}
+
+const InventoryItem = ({ heading, color, value }: InventoryProps) => {
+  return (
+    <div className="flex">
+      <div className="m-1 basis-1/4">{heading}</div>
+      <div className="basis-2/4 m-1">
+        <div
+          className="px-2 rounded-lg"
+          style={{ backgroundColor: color, width: `${value}%` }}
+        ></div>
+      </div>
+      <div className="basis-1/4">{value}%</div>
     </div>
   );
 };
